c1fc2e1587ee49d0b525a639294c64407d302f21
Pedro Lage <lagecomg@gmail.com>
2025-02-03 16:22:50 -0300

    Analisando o valor médio do aluguel dos imóveis

--- main.py
+++ main.py
@@ -27,3 +27,32 @@ print(dados['Tipo'])
 print(dados[['Quartos', 'Valor']])
 
 """
+
+# Realizando a análise exploratória dos dados
+
+colunasIniciais = dados.head()
+print(colunasIniciais)
+
+## Arredondamento dos valores com round
+
+round(dados['Valor'].mean(), 2)
+
+# Método goupby()
+## Passando a coluna Tipo para a partir dela agrupar os dados
+## numeric_only=True para a média ser feita só nos valores numéricos
+
+round(dados.groupby('Tipo').mean(numeric_only=True), 2)
+
+## numeric_only=True excluído pois não há mais necessidade
+
+## Colchete duplo para criar um dataframe
+## sortt_values('coluna') para organizar os valores do menor para o maior
+round(dados.groupby('Tipo')[['Valor']].mean().sort_values('Valor'), 2)
+
+# Criando um gráfico para a visualização
+
+import matplotlib.pyplot as plt
+
+df_preco_tipo = round(dados.groupby('Tipo')[['Valor']].mean().sort_values('Valor'), 2)
+df_preco_tipo.plot(kind='barh', figsize=(14, 10), color = 'purple')
+plt.show()
